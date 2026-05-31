76d1b7d87bb3cb3992d421d3283c1b360631158b
Ashis Shaw <ashisshaw1811@gmail.com>
2026-05-31 12:35:23 +0530

    Fix import statement for Chroma vector store

--- rag.py
+++ rag.py
@@ -1,7 +1,8 @@
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from langchain_community.vectorstores import Chroma
+# from langchain_community.vectorstores import Chroma
+from langchain.vectorstores import Chroma
 
 from langchain_openai import OpenAIEmbeddings
 
